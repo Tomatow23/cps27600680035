8a8c68543b3fa5d3c5511f1e6f5c202d9927825d
Nathan Dunn <tomatow21@gmail.com>
2022-10-19 22:24:16 -0400

    git commit -m "commit2"

--- assignments/assignment4/add_name.php
+++ assignments/assignment4/add_name.php
@@ -3,9 +3,10 @@ class Name{
     public $name;
     function addName($name){
         $this->name = $name;
-        $x=explode(" ", $name);
-        $y = array("lname"=>$x[1],"fname"=>$x[0]);
-        $z = implode(", ", $y);
+        $z = implode(", ", array_reverse(explode(" ", $name)));
+        // $x=explode(" ", $name);
+        // // $y = array("lname"=>$x[1],"fname"=>$x[0]);
+        // $z = implode(", ", $y);
         return $z . "\n";
     }
 }
